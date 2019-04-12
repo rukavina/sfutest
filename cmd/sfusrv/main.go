6cfcb3d7c5776cb69e67c1dc67c5040c63eb8cf5
Milan Rukavina <milan@horisen.com>
2019-04-12 15:55:19 +0200

    sfu struct

--- cmd/sfusrv/main.go
+++ cmd/sfusrv/main.go
@@ -33,67 +33,102 @@ func main() {
 	m.RegisterCodec(webrtc.NewRTPVP8Codec(webrtc.DefaultPayloadTypeVP8, 90000))
 	api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
 
-	offer := webrtc.SessionDescription{}
+	sfu := &SFU{
+		answerChan: answerChan,
+		api:        api,
+		pcConfig:   peerConnectionConfig,
+	}
 
+	offer := webrtc.SessionDescription{}
 	log.Printf("SDP srv up and running @ http://localhost:%d, waiting for an offer\n\n", *port)
 	signal.Decode(<-offerChan, &offer)
 	log.Println("SDP offer received")
 
-	// Create a new RTCPeerConnection
-	publishPC, err := api.NewPeerConnection(peerConnectionConfig)
+	err := sfu.createPublisherPC(offer)
 	if err != nil {
-		panic(err)
+		log.Printf("Error creating Publisher peer connection %v", err)
+	}
+
+	log.Println("Waiting for new local track")
+
+	for {
+		fmt.Println("")
+		fmt.Println("Curl an base64 SDP to start sendonly peer connection")
+
+		recvOnlyOffer := webrtc.SessionDescription{}
+
+		log.Println("Waiting for new receive only sdp offer")
+		signal.Decode(<-offerChan, &recvOnlyOffer)
+		err = sfu.createViewerPC(recvOnlyOffer)
+		if err != nil {
+			log.Printf("Error creating Publisher peer connection %v", err)
+		}
+	}
+}
+
+type SFU struct {
+	api        *webrtc.API
+	publishPC  *webrtc.PeerConnection
+	pcConfig   webrtc.Configuration
+	answerChan chan string
+}
+
+func (s *SFU) createPublisherPC(offer webrtc.SessionDescription) error {
+	// Create a new RTCPeerConnection
+	var err error
+	s.publishPC, err = s.api.NewPeerConnection(s.pcConfig)
+	if err != nil {
+		return err
 	}
 
 	// Allow us to receive 1 video track
-	if _, err = publishPC.AddTransceiver(webrtc.RTPCodecTypeVideo); err != nil {
-		panic(err)
+	if _, err = s.publishPC.AddTransceiver(webrtc.RTPCodecTypeVideo); err != nil {
+		return err
 	}
 
 	log.Println("New Publisher PeerConnection created and added Transceiver")
 
-	localTrackChan := make(chan *webrtc.Track)
 	// Set the remote SessionDescription
-	err = publishPC.SetRemoteDescription(offer)
+	err = s.publishPC.SetRemoteDescription(offer)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	// Create answer
-	answer, err := publishPC.CreateAnswer(nil)
+	answer, err := s.publishPC.CreateAnswer(nil)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	// Sets the LocalDescription, and starts our UDP listeners
-	err = publishPC.SetLocalDescription(answer)
+	err = s.publishPC.SetLocalDescription(answer)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	log.Println("peerConnection offer accepted and answer created")
-	answerChan <- signal.Encode(answer)
+	s.answerChan <- signal.Encode(answer)
 
 	// Set a handler for when a new remote track starts, this just distributes all our packets
 	// to connected peers
-	publishPC.OnTrack(func(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) {
+	s.publishPC.OnTrack(func(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) {
 		// Send a PLI on an interval so that the publisher is pushing a keyframe every rtcpPLIInterval
 		// This can be less wasteful by processing incoming RTCP events, then we would emit a NACK/PLI when a viewer requests it
 		go func() {
 			ticker := time.NewTicker(rtcpPLIInterval)
 			for range ticker.C {
-				if rtcpSendErr := publishPC.WriteRTCP([]rtcp.Packet{&rtcp.PictureLossIndication{MediaSSRC: remoteTrack.SSRC()}}); rtcpSendErr != nil {
+				if rtcpSendErr := s.publishPC.WriteRTCP([]rtcp.Packet{&rtcp.PictureLossIndication{MediaSSRC: remoteTrack.SSRC()}}); rtcpSendErr != nil {
 					fmt.Println(rtcpSendErr)
 				}
 			}
 		}()
 
 		// Create a local track, all our SFU clients will be fed via this track
-		localTrack, newTrackErr := publishPC.NewTrack(remoteTrack.PayloadType(), remoteTrack.SSRC(), "video", "pion")
-		if newTrackErr != nil {
-			panic(newTrackErr)
+		localTrack, err := s.publishPC.NewTrack(remoteTrack.PayloadType(), remoteTrack.SSRC(), "video", "pion")
+		if err != nil {
+			log.Printf("On track error creating new track %v", err)
 		}
-		localTrackChan <- localTrack
+		s.localTrackChan <- localTrack
 
 		log.Println("New remote track and its local track created")
 
@@ -111,47 +146,40 @@ func main() {
 		}
 	})
 
-	log.Println("Waiting for new local track")
-	localTrack := <-localTrackChan
-	for {
-		fmt.Println("")
-		fmt.Println("Curl an base64 SDP to start sendonly peer connection")
+	return nil
+}
 
-		recvOnlyOffer := webrtc.SessionDescription{}
-
-		log.Println("Waiting for new receive only sdp offer")
-		signal.Decode(<-offerChan, &recvOnlyOffer)
-
-		// Create a new PeerConnection
-		viewerPC, err := api.NewPeerConnection(peerConnectionConfig)
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = viewerPC.AddTrack(localTrack)
-		if err != nil {
-			panic(err)
-		}
-
-		// Set the remote SessionDescription
-		err = viewerPC.SetRemoteDescription(recvOnlyOffer)
-		if err != nil {
-			panic(err)
-		}
-
-		// Create answer
-		answer, err := viewerPC.CreateAnswer(nil)
-		if err != nil {
-			panic(err)
-		}
-
-		// Sets the LocalDescription, and starts our UDP listeners
-		err = viewerPC.SetLocalDescription(answer)
-		if err != nil {
-			panic(err)
-		}
-
-		log.Println("Offer received, new PeerConnection created, localTrack added and answer created")
-		answerChan <- signal.Encode(answer)
+func (s *SFU) createViewerPC(offer webrtc.SessionDescription) error {
+	// Create a new PeerConnection
+	viewerPC, err := s.api.NewPeerConnection(s.pcConfig)
+	if err != nil {
+		return err
 	}
+
+	_, err = viewerPC.AddTrack(localTrack)
+	if err != nil {
+		return err
+	}
+
+	// Set the remote SessionDescription
+	err = viewerPC.SetRemoteDescription(offer)
+	if err != nil {
+		return err
+	}
+
+	// Create answer
+	answer, err := viewerPC.CreateAnswer(nil)
+	if err != nil {
+		return err
+	}
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	err = viewerPC.SetLocalDescription(answer)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Offer received, new PeerConnection created, localTrack added and answer created")
+	s.answerChan <- signal.Encode(answer)
+	return nil
 }
